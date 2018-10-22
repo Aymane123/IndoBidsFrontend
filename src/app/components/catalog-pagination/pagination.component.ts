e873cb21634740ece9ac29a375cb1366ccd066c2
ismailbe <ismail.belkasem@student.kdg.be>
2018-10-22 11:22:28 +0800

    laatste

--- src/app/components/catalog-pagination/pagination.component.ts
+++ src/app/components/catalog-pagination/pagination.component.ts
@@ -31,7 +31,7 @@ export class PaginationComponent {
   sort(event) {
     if (event != null) {
       this.key = event.target.value;
-      console.log(event.target.value);
+      console.log(this.key);
       this.reverse = !this.reverse;
     }
   }
